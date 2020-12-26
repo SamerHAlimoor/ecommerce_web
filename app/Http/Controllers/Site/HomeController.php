45f4100758d12d5193014afda6177c803d201b59
SamerAlimoor <sipar.alamour@gmail.com>
2020-12-26 23:47:01 +0200

    =26-12-2020--9:30 PM login and register

---
+++ app/Http/Controllers/Site/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminLoginRequest;
+
+class HomeController extends Controller
+{
+    //
+   
+}
